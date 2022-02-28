eca58bf343a62e8793565726cfd68aaad148a882
mariorzgz <mario.rod.gonz97@gmail.com>
2022-02-28 15:46:31 +0100

    Added all associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
 
   has_many :receiving_orders, foreign_key: "desired_item_id", class_name: 'Orders'
   has_many :offering_orders, foreign_key: "offered_item_id", class_name: 'Orders'
+
+  has_many :item_outfits
+  has_many :outfits, through: :item_outfits
 end
--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,6 +1,6 @@
 class Outfit < ApplicationRecord
   belongs_to :user
 
-  has_many :item_outfit
-  has_many :items, through: item_outfit
+  has_many :item_outfits
+  has_many :items, through: :item_outfits
 end
