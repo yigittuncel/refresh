db5a28815adcb021bafd1d78902187201450c380
yigit tuncel <yigittuncel35@gmail.com>
2022-03-01 16:51:59 +0100

    create item page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @item = Item.new(params_item)
+    @item.user = current_user
+    if @item.save
+      redirect_to :root
+    else
+      render :new
+    end
   end
 
+  private
+
+  def params_item
+    params.require(:item).permit(:name, :description, :brand, :size, :color, :photo)
+  end
 end
