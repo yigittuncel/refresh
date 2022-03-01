4f59a46cf72ff6c782df070040524fd9dcdbcf41
mariorzgz <mario.rod.gonz97@gmail.com>
2022-03-01 14:54:56 +0100

    Added the layout to the feed; made some changes to the outfit cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:feed]
 
   def feed
-    @outfits = Outfit.all
+    @outfits = Outfit.all.order('created_at DESC')
+    @users = User.all
   end
 end
