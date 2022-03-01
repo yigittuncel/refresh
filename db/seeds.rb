d5314726b32a66546a63c4e67fc9101cb19975da
mariorzgz <mario.rod.gonz97@gmail.com>
2022-03-01 09:35:16 +0100

    Fixed typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,7 +54,7 @@ item_descriptions = [
   "Socks in a soft, fine-knit cotton blend with elasticated tops.",
   "The Hoodie. Hoodie in sweatshirt fabric made from a cotton blend."
 ]
-item_brands = ["H&M", "Zara", "Bershka", "Nike", "Gucci", "	Levi’s"]
+item_brands = ["H&M", "Zara", "Bershka", "Nike", "Gucci", "Levi’s"]
 item_sizes = ["XS", "S", "M", "L", "XL", "XXL"]
 item_colors = ["brown", "light grey", "mint blue", "yellow", "blue", "pink"]
 
