338060efc2d31d6839c102a78b7eb834a9a489f5
Christina Borensky <christina.borensky@web.de>
2022-03-01 14:53:45 +0100

    Routes & Controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :set_items, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_items
+    @item = Item.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#feed'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items, only: [:index, :show, :new, :create] do
+    resources :orders, only [:create]
+  end
 end
