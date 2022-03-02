a79c931a1db5c239a87e1100b0571264fdb4aff3
yigit tuncel <yigittuncel35@gmail.com>
2022-03-02 12:29:53 +0100

    outfit form with item

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,2 +1,36 @@
 class OutfitsController < ApplicationController
+  def new
+    @outfit = Outfit.new
+    @items = current_user.items
+  end
+
+  def create
+    @outfit = Outfit.new(params_outfit)
+    @user = current_user
+    @outfit.user = @user
+    if @outfit.save
+      ItemOutfit.create(
+        outfit: @outfit,
+        item: Item.find(params[:outfit][:items])
+      )
+      redirect_to :root
+
+    else
+      render :new
+    end
+
+    # @item_outfit = ItemOutfit.new(params_item_outfit)
+    # @item_outfit.outfit = @outfit
+    # if @item_outfit.save
+    #   redirect_to :root
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+
+  def params_outfit
+    params.require(:outfit).permit(:description, :photo)
+  end
 end
