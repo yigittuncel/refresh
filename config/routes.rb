25290182cc36b176366356034a2b9c07fa54f0c0
Christina Borensky <christina.borensky@web.de>
2022-03-02 16:13:57 +0100

    conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :items, only: %i[new create show]
+  resources :outfits, only: %i[new create]
   get "/dashboard", to: "pages#dashboard"
 end
