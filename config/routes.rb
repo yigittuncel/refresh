12a4886869a1983e3251538d4341e652fff76d2f
yigit tuncel <yigittuncel35@gmail.com>
2022-03-07 15:06:01 +0100

    inbox page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :outfits, only: %i[new create show]
   get "/dashboard", to: "pages#dashboard"
   get "/trades", to: "pages#trades"
-  resources :chatrooms, only: %i[show] do
+  resources :chatrooms, only: %i[index show] do
     resources :messages, only: :create
   end
 end
